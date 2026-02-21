91cbe0945e3c3a2ec5b0ee7a88492ec4acde38a9
TG-MSMods <movieplus9090@gmail.com>
2026-02-21 11:40:20 +0530

    Initial push for kamanime-api

---
+++ src/config/config.js
@@ -0,0 +1,18 @@
+const config = {
+  baseurl: 'https://hianime.do',
+  baseurl_v2: 'https://kaido.to',
+  providers: 'https://megacloud.club',
+
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+    'Accept': '*/*',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Referer': 'https://hianime.do/',
+    'X-Requested-With': 'XMLHttpRequest',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'cross-site'
+  },
+};
+
+export default config;
